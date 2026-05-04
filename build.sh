dc18042ffffa5115545c73b75ca10db981329ef9
Rosemary Dimakunne <dimakunnerosemaryc@gmail.com>
2026-05-04 05:08:50 -0500

    Fix build: clear projects before reloading fixture

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from main.models import Project; Project.objects.all().delete()"
 python manage.py loaddata main/fixtures/projects.json
