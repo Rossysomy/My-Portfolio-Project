87afe80638b7442717a028b6d0ecf8eb10f81afd
Rosemary Dimakunne <dimakunnerosemaryc@gmail.com>
2026-05-04 00:56:32 -0500

    Fix media file serving via WhiteNoise on Render

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -1,5 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
+
 application = get_wsgi_application()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
+application.add_files(str(BASE_DIR / 'media'), prefix='media')
