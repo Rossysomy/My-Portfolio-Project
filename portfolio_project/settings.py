53684df9f6ac87bd9de8eccf2655bf559a507fae
Rosemary Dimakunne <dimakunnerosemaryc@gmail.com>
2026-05-04 05:00:17 -0500

    Serve media via WhiteNoise static pipeline

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -77,11 +77,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static', BASE_DIR / 'media']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
